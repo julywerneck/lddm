c26f6fc763f10a6a08de9998b84b031ac622533a
July Ferreira Murta Werneck <1202537@sga.pucminas.br>
2022-04-12 10:12:49 -0300

    adding container

--- app_widget/lib/main.dart
+++ app_widget/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: const MyApp());
+  runApp(const MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -14,8 +14,34 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [Text("texto1"), Text("texto2"), Text("texto3")],
-    );
+    return Container(
+        child: Column(
+      children: [
+        Row(
+          children: [
+            formatarTexto("T 1"),
+            formatarTexto("T 2"),
+            formatarTexto("T 3"),
+          ],
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+        ),
+        ElevatedButton(onPressed: () {}, child: const Text("Clique ai"))
+      ],
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisAlignment: MainAxisAlignment.center,
+    ));
+  }
+
+  formatarTexto(String valor) {
+    return Text(valor,
+        style: const TextStyle(
+            fontSize: 30,
+            color: Color.fromARGB(255, 245, 3, 205),
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 20,
+            wordSpacing: 20,
+            decoration: TextDecoration.none));
   }
 }
