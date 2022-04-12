2484f25c187a84b44e95713caafc1fcda7aeb2ed
July Ferreira Murta Werneck <1202537@sga.pucminas.br>
2022-04-12 10:28:07 -0300

    last

--- app_widget/lib/main.dart
+++ app_widget/lib/main.dart
@@ -15,28 +15,31 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Container(
+        padding: const EdgeInsets.all(10),
+        color: Colors.white,
+        margin: const EdgeInsets.only(left: 30),
         child: Column(
-      children: [
-        Row(
           children: [
-            formatarTexto("T 1"),
-            formatarTexto("T 2"),
-            formatarTexto("T 3"),
+            Row(
+              children: [
+                formatarTexto("T 1"),
+                formatarTexto("T 2"),
+                formatarTexto("T 3"),
+              ],
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+            ),
+            ElevatedButton(onPressed: () {}, child: const Text("Clique ai"))
           ],
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-        ),
-        ElevatedButton(onPressed: () {}, child: const Text("Clique ai"))
-      ],
-      crossAxisAlignment: CrossAxisAlignment.end,
-      mainAxisAlignment: MainAxisAlignment.center,
-    ));
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.center,
+        ));
   }
 
   formatarTexto(String valor) {
     return Text(valor,
         style: const TextStyle(
-            fontSize: 30,
+            fontSize: 20,
             color: Color.fromARGB(255, 245, 3, 205),
             fontStyle: FontStyle.italic,
             fontWeight: FontWeight.bold,
