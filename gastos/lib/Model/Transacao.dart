f72f51f9c40bcf02e3c062b1b81757f97712bec2
July Ferreira Murta Werneck <1202537@sga.pucminas.br>
2022-05-03 10:26:47 -0300

    null safety

--- gastos/lib/Model/Transacao.dart
+++ gastos/lib/Model/Transacao.dart
@@ -1,7 +1,13 @@
+import 'package:flutter/foundation.dart';
+
 class Transacao {
-  String id;
-  String titulo;
-  double quantidade;
-  DateTime data;
-  Transacao({this.id, this.titulo, this.quantidade, this.data}) {}
+  String? id;
+  String? titulo;
+  double? quantidade;
+  DateTime? data;
+  Transacao(
+      {@required this.id,
+      @required this.titulo,
+      @required this.quantidade,
+      @required this.data}) {}
 }
