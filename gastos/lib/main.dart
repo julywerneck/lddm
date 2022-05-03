0f2567344169009e5f62a5960868f209eeaa67c9
July Ferreira Murta Werneck <1202537@sga.pucminas.br>
2022-05-03 10:30:36 -0300

    list

--- gastos/lib/main.dart
+++ gastos/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Model/Transacao.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,6 +22,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Transacao> transacoes = [
+    Transacao(id: "A1", titulo: "Café", quantidade: 4.5, data: DateTime.now()),
+    Transacao(id: "A2", titulo: "Leite", quantidade: 4.2, data: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
